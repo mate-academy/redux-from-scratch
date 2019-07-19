5c1d7e4bfc64ced8c9f227d99b8804eeb977f4ce
mgrinko <glukym@gmail.com>
2019-07-19 11:23:47 +0300

    CIrcle and Box were added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
 import React from 'react';
 
 const App = () => (
-  <div className="App">
-    <h3>App</h3>
-    <Modal children={123}/>
-  </div>
+  <>
+    <Box text={123} bgColor="red" />
+    <Circle text={456} bgColor="red" />
+    <Circle text={789} bgColor="yellow" />
+  </>
 );
 
-const Modal = (props) => (
-  <article>
-    <header>Modal</header>
-    <section>{props.children}</section>
+const Box = (props) => (
+  <article style={{ backgroundColor: props.bgColor }}>
+    {props.text}
   </article>
 );
 
 
-const List = (props) => {
-  return (
-    <ul>
-      {props.items.map(item => (
-        <li>{item}</li>
-      ))}
-    </ul>
-  );
-};
+const Circle = (props) => (
+  <section style={{ backgroundColor: props.bgColor }}>
+    {props.text}
+  </section>
+);
+
+
+export default App;
+
+
+// const App = () => (
+//   <div className="App">
+//     <h3>App</h3>
+//     <Modal>
+//       <div>{123}</div>
+//       <b>456</b>
+//       <p>789</p>
+//     </Modal>
+//   </div>
+// );
+// const Modal = (props) => {
+//   console.log(props.children);
+//   return (
+//     <article>
+//       <header>Modal</header>
+//       <section>{props.children}</section>
+//     </article>
+//   );
+// };
 
 // //
 // class CounterApp extends React.Component{
@@ -50,6 +70,50 @@ const List = (props) => {
 //   <h2>Child count {props.count}</h2>
 // );
 
-export default App;
+// const App = () => (
+//   <div className="App">
+//     <h3>App</h3>
+//
+//     <DataTable
+//       items={todos}
+//       header={() => (
+//         <tr>
+//           <th>ID</th>
+//           <th>Title</th>
+//           <th>User</th>
+//         </tr>
+//       )}
+//       renderRow={(item) => (
+//         <tr>
+//           <td>{item.id}</td>
+//           <td>{item.title}</td>
+//           <td>{item.user.name}</td>
+//         </tr>
+//       )}
+//     />
+//
+//     <List
+//       items={[1, 2, 3]}
+//       renderItem={(item) => (
+//         <i>** {item}</i>
+//       )}
+//     />
+//   </div>
+// );
+//
+//
+// const List = (props) => {
+//   return (
+//     <ul>
+//       {props.items.map(item => (
+//         <li key={item}>
+//           {props.renderItem(item)}
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
+
+
 
 
