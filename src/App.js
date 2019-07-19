8c397dd49024418cc5f67f0b0178804a70ad06f7
mgrinko <glukym@gmail.com>
2019-07-19 10:19:43 +0300

    Counter example was added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-const App = () => (
-  <div className="App">
-    <h1>Redux from scratch</h1>
-  </div>
-);
+class App extends React.Component{
+  state = { count: 0 };
+
+  increase = () => {
+    this.setState(state => ({
+      count: state.count + 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>App count {this.state.count}</h1>
+        <button onClick={this.increase}>Add</button>
+        <Child count={this.state.count}/>
+      </div>
+    );
+  }
+}
+
+const Child = ({ count }) => <h2>Child count {count}</h2>;
 
 export default App;
